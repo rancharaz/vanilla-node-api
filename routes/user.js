d27082a7fac9d2c3dc7d446d8da7b5e7c3cf8d31
unknown <ranveer1990@hotmail.com>
2022-10-04 12:03:58 +0400

    change files

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var db = require('./db');
 
-db.query('SELECT ? FROM t_user',query, function(err, results, query) {
+db.query('SELECT * FROM user',query, function(err, results, query) {
     if (err) throw err;
     if(!err) {
         indexPage.receiveResults(results);
